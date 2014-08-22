26d35cf537ea8baa81ada21e660cc07ea1c490a8
Srdjan Bulic <srdjanbulic@gmail.com>
2014-08-23 00:07:07 +0200

    Switch test to hello jenkins

--- test/test.js
+++ test/test.js
@@ -3,9 +3,11 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('hello jenkins', done);
   });
-});var request = require('supertest');
+});
+
+var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
